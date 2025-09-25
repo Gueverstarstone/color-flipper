d9bdfbabd38de70efb2374de51517730967f2a9d
gueverstarstone <josephmungai028@gmail.com>
2025-09-26 02:11:54 +0300

    Added a random color generator

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ function changeBg(name) {
 changeBg('black') */
 
 //Adding event Listeners to buttons
+//get an element by its ID name
 document.getElementById('yellow').addEventListener('click', () => changeBg('yellow'));
 document.getElementById('green').addEventListener('click', () => changeBg('green'));
 document.getElementById('blue').addEventListener('click', () => changeBg('blue'));
+
+function randomColor() {
+  // Generate a random number between 0 and 255 for the red channel
+  const yellow = Math.round(Math.random() * 255)
+  const green = Math.round(Math.random() * 255)
+  const blue = Math.round(Math.random() * 255)
+
+  // Build an RGB string like "rgb(123, 45, 200)"
+  const colors = `rgb(${yellow}, ${green}, ${blue})`;
+
+  // Apply this random color to the page background
+  bodyType.style.backgroundColor = colors;
+}
+//call the function
+console.log(randomColor());
